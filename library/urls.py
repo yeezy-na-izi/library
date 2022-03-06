e06b0363a19af868c52386476e20cd0ad6761385
yeezy_na_izi <howone1sky@gmail.com>
2022-03-07 02:01:53 +0300

    add

--- library/urls.py
+++ library/urls.py
@@ -3,4 +3,5 @@ from library import views
 
 urlpatterns = [
     path('', views.home_page, name='home_page'),
+    path('books/', views.home_page, name='books'),
 ]
--- library/views.py
+++ library/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from library.models import Book
+from library.models import TagsType
 
 
 def home_page(request):
@@ -6,3 +8,10 @@ def home_page(request):
     print(request.user.is_superuser)
     return render(request, 'library/home_page/index.html', context)
 
+
+def books(request):
+    context = {
+        'books': Book.objects.all(),
+        'Tags': Book.objects.all()
+    }
+    return render(request, 'library/books/index.html', context)
